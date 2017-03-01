1931df72c611c9480469e60ebed0c19281a1ece9
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2017-03-01 17:42:47 -0500

    set up forms for app/public/survey.html, button for home.html

---
+++ server.js
@@ -0,0 +1,9 @@
+// //DEPENDENCIES
+// //===============================
+// 	//require express
+// 	//require body-parser
+// 	//require path
+// //Express Configuration
+// var app = express()
+// //set up port
+// app.use(bodyParser.json());
