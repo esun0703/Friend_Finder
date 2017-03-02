f366528b56d84031346daa113ff55f91db236038
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2017-03-01 20:24:12 -0500

    home.html: added container, survey.html:added container, added modal

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+//Require Data
+//../data/friends
+
+//Routing
+//module.exports = function(app){
+	//Get requests
+		//GET route: "/api/friends": display a JSON of all possible
+	//post requests
+		//POST "/api/friends" handle incoming survey results
+// }
+
+
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,14 @@
+//=====================================
+//DEPENDENCIES
+//=====================================
+//require path
+
+//=====================================
+//ROUTING
+//=====================================
+
+//Modules.exports = function(app){
+
+	//GET route to /survey: display survey page
+	//Default USE route goes to home
+// }
